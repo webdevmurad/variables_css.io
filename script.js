dbc2b048fefe19347bda220d9f22cf5a684ec435
Murad <murad-arslangiraev-1994@mail.ru>
2019-10-16 19:36:18 +0300

    забыл полседнюю строчку

--- script.js
+++ script.js
@@ -6,4 +6,4 @@ const inputs = document.querySelectorAll('.controls input');
     }
 
     inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));п
